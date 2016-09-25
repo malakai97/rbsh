39a79ec0c218ac7ed656366ba886c6bfdcc0e0cf
Bryan Hockey <bhock@umich.edu>
2016-09-25 18:33:01 -0500

    progress, still using common word, whitespace rules

--- lib/rbsh/lexer.rb
+++ lib/rbsh/lexer.rb
@@ -21,13 +21,28 @@ module RBSH
     rule(/`/, :default)       { push_state(:subshell); :SUBSHELL_START }
     rule(/"/, :default)       { push_state(:double_quote); :DOUBLE_QUOTE_START }
     rule(/'/, :default)       { push_state(:single_quote); :SINGLE_QUOTE_START }
+    rule(/\|/, :default)      { :PIPE }
+    rule(/\{/, :default)      { push_state(:curly_brace); :CURLY_BRACE_START }
+    rule(/\[/, :default)      { push_state(:bracket); :BRACKET_START }
+
+
+    rule(/\]/, :bracket)      { pop_state; :BRACKET_END}
+    rule(/"/, :bracket)       { push_state(:double_quote); :DOUBLE_QUOTE_START }
+    rule(/'/, :bracket)       { push_state(:single_quote); :SINGLE_QUOTE_START }
+    rule(/`/, :bracket)       { push_state(:subshell); :SUBSHELL_START }
+
+
+    rule(/\}/, :curly_brace)  { pop_state; :CURLY_BRACE_END }
+    rule(/"/, :curly_brace)   { push_state(:double_quote); :DOUBLE_QUOTE_START }
+    rule(/'/, :curly_brace)   { push_state(:single_quote); :SINGLE_QUOTE_START }
+    rule(/`/, :curly_brace)   { push_state(:subshell); :SUBSHELL_START }
 
     rule(/\\/, :single_quote) { push_state(:escape); :BACKSLASH }
     rule(/'/, :single_quote)  { pop_state; :SINGLE_QUOTE_END }
 
     rule(/\\/, :double_quote) { push_state(:escape) }
     rule(/"/, :double_quote)  { pop_state; :DOUBLE_QUOTE_END }
-    rule(/[^\\]#\{/, :double_quote) { push_state(:interpolate); :INTERPOLATE_START }
+    rule(/#\{/, :double_quote) { push_state(:interpolate); :INTERPOLATE_START }
 
     rule(/"/, :interpolate)   { push_state(:double_quote); :DOUBLE_QUOTE_START }
     rule(/'/, :interpolate)   { push_state(:single_quote); :SINGLE_QUOTE_START }
@@ -37,19 +52,32 @@ module RBSH
 
     rule(/\\/, :subshell)     { push_state(:escape); :BACKSLASH }
     rule(/`/, :subshell)      { pop_state; :SUBSHELL_END }
-    rule(/[^\\]#\{/, :subshell) { push_state(:interpolate); :INTERPOLATE_START }
+    rule(/#\{/, :subshell)    { push_state(:interpolate); :INTERPOLATE_START }
     rule(/"/, :subshell)      {|c| push_state(:double_quote); [:ANY, c] }
     rule(/'/, :subshell)      {|c| push_state(:single_quote); [:ANY, c] }
 
-    [:default, :subshell, :interpolate, :double_quote, :single_quote].each do |state|
-      rule(/.|\s/, state)     {|c| [:ANY, c] }
+
+    [
+      :default,
+      :subshell,
+      :interpolate,
+      :double_quote,
+      :single_quote,
+      :curly_brace,
+      :bracket
+    ].each do |state|
+      rule(/[^\s]/, state)    {|c| [:CHAR, c] }
+      rule(/\s/, state)       {|c| [:WHITESPACE, c] }
     end
   end
 
 end
 
+File.readlines("/home/bhock/code/rbsh/some_commands").each do |line|
+  line = line.strip
+  puts "######## #{line} ########"
+  result =  RBSH::Lexer.new.lex(line)
+  puts result
+end
 
-
-result =  RBSH::Lexer.new.lex('"che#{5.times("foo"}ese"')
-puts result
-require 'pry'; binding.pry
+# require 'pry'; binding.pry
