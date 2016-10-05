495f40bd0841210c992f5ba0f840f2b7cb0cf629
Bryan Hockey <bhock@umich.edu>
2016-10-05 17:09:57 -0500

    tuck which into rbsh.rb for now

--- lib/rbsh.rb
+++ lib/rbsh.rb
@@ -1,6 +1,25 @@
-require "rbsh/parser"
+require "rltk"
+
 require "rbsh/lexer"
+require "rbsh/ast"
+require "rbsh/parser"
+
 
 module RBSH
+  private
 
+  # Cross-platform way of finding an executable in the $PATH.
+  # Stolen from http://mislav.net/ via stackoverflow
+  #
+  #   which('ruby') #=> /usr/bin/ruby
+  def which(cmd)
+    exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
+    ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
+      exts.each { |ext|
+        exe = File.join(path, "#{cmd}#{ext}")
+        return exe if File.executable?(exe) && !File.directory?(exe)
+      }
+    end
+    return nil
+  end
 end
